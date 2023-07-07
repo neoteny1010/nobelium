3231c679a7d13d9bafeb41b552409628b8483f47
neoteny1010 <138466478+neoteny1010@users.noreply.github.com>
2023-07-07 17:55:22 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: 'Dolce far niente',
+  author: 'Haku LEE',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
