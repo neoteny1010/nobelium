faaaf8d1f4861f8c863c32c94ea041404f523ec9
neoteny1010 <138466478+neoteny1010@users.noreply.github.com>
2023-07-07 18:18:48 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,12 +3,12 @@ const BLOG = {
   author: 'Haku LEE',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: 'sweet idleness',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'Pretendard', // ['sans-serif', 'serif']
-  lightBackground: '#333333', // use hex value, don't forget '#' e.g #fffefc
+  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2023, // If leave this empty, current year will be used.
